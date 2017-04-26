e39d72a460409252719ff1ae6aa34c6c5f357457
lly <742142833@qq.com>
2017-04-26 19:58:27 +0800

    mongoose default set get

--- server/db/insert.js
+++ server/db/insert.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose')
  require('./model.js')
 var User = mongoose.model('user')
-var name = new User({
-  name:'kkk'
+var name = new User()
+name.save(function(err){
+	if(err){
+		console.log(err);
+		return
+	}
+	console.log(name.name);
 })
-name.save()
+ 
--- server/db/model.js
+++ server/db/model.js
@@ -1,9 +1,29 @@
 var mongoose = require('mongoose')
 var db = require('./index.js')
 var UserSchema = new mongoose.Schema({
-  name:String,
+  name:{
+  	type:String,
+  	default:'new name',
+  	trim:true,
+  	set:function(name){
+  		if(!name) return 
+  		 if(0!==name.indexof('lly')){
+  		 	name = 'lly'+'new name'
+  		 }
+  		 return name 
+  	},
+  	get:function(name){
+  		if(!name) return 
+  			if(name!=='lly'){
+  				name = 'seuic'+name
+  			}else{
+  				name='seuic'
+  			}
+  			return name 
+  	}
+  },
   age:Number,
-  login:{type:Date,default:new Date()}
+  login:{type:Date,default:Date.now}
 })
 
 var User = mongoose.model('user',UserSchema)
@@ -11,4 +31,4 @@ var User = mongoose.model('user',UserSchema)
 //     name : 'T'                 //用户账号
 // });
 //  user.save()
-module.exports = user
+module.exports = User
